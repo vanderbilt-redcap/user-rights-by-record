b9071799ff870c7a6ee55d575aebedf54a9c6440
moorejr5 <james.r.moore@vanderbilt.edu>
2018-09-17 12:04:01 -0500

    Updated 'redcap_module_link_check_display' to use new parameters.

--- UserRightsByRecordExternalModule.php
+++ UserRightsByRecordExternalModule.php
@@ -362,7 +362,7 @@ class UserRightsByRecordExternalModule extends AbstractExternalModule
 		return $returnArray;
 	}
 
-	function redcap_module_link_check_display($project_id, $link, $record, $instrument, $instance, $page) {
+	function redcap_module_link_check_display($project_id, $link) {
 		if(\REDCap::getUserRights(USERID)[USERID]['design'] == '1'){
 			return $link;
 		}
